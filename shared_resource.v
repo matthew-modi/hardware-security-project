16ecd214a286aad90049680a83bdaf5e4b9795af
sps2308 <sps2308@cadpc35.ee.columbia.edu>
2025-05-06 16:06:13 -0400

    just starting, made basic shared_resource module

---
+++ shared_resource.v
@@ -0,0 +1,19 @@
+module shared_resource(
+	input logic clk,
+        input logic reset,
+        input logic [31:0] resource_input,
+        output logic [31:0] resource_output
+);
+
+//imagine this like memory, where the data at the address is just the address plus 10 for now
+
+	always @ (posedge clk) begin
+
+		if (reset) begin
+			resource_output <= 0;
+		end else begin
+			resource_output <= resource_input + 32'd10;
+		end
+	end
+
+endmodule

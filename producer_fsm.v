88c0f904d297e780b66dcb23300ce9529db50637
sps2308 <sps2308@cadpc35.ee.columbia.edu>
2025-05-06 16:59:57 -0400

    very basic producer_fsm that doesn't incorporate valid or flush yet

--- producer_fsm.v
+++ producer_fsm.v
@@ -10,9 +10,33 @@ module producer_fsm (
 );
 // Producer logic placeholder
 
-logic 
+	parameter S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4, S5 = 5, S6 = 6, S7 = 7;
 
-always_ff (@ posedge clk) begin
+	logic [2:0] state, next_state;
 
+	always_comb begin //no input? is invalid used here (its an input to the pipeline and producer)?
+		case(state)
+			S0 : next_state = S1;
+			S1 : next_state = S2;
+			S2 : next_state = S3;
+			S3 : next_state = S4;
+			S4 : next_state = S5;
+			S5 : next_state = S6;
+			S6 : next_state = S7;
+			S7 : next_state = S0;
+			default next_state = S0;
+		endcase
+	end
+
+	always_ff (@ posedge clk) begin
+		if (reset) begin
+			state <= 3'b0;
+		end else begin
+			state <= next_state;
+		end
+	end
+
+	assign pipeline1_inputs = (reset) ? 32'b0 : 32'h10000 << state;
+	assign pipeline2_inputs = (reset) ? 32'b0 : 32'h10000 >> state;
 
 endmodule

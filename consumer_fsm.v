8de5c129619efa5fad8055a0660e28fb79cf240b
sps2308 <sps2308@cadpc35.ee.columbia.edu>
2025-05-06 19:06:41 -0400

    simple consumer, only looks at pipeline results rn

--- consumer_fsm.v
+++ consumer_fsm.v
@@ -6,4 +6,37 @@ module consumer_fsm (
     input  wire [1:0]  valid
 );
 // Consumer logic placeholder
+	//add more states for more possibilities
+	parameter S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4, S5 = 5, S6 = 6, S7 = 7;
+
+	logic [2:0] state, next_state;
+
+	logic [31:0] received_input1;
+	logic [31:0] received_input2;
+
+	assign received_input1 = (valid[0]) ? pipeline1_outputs : {pipeline1_outputs[31:5], 5'h1f};
+	assign received_input2 = (valid[1]) ? pipeline2_outputs : {pipeline2_outputs[31:5], 5'h1f};
+
+	always_comb begin
+		if (reset) begin
+			next_state = S0;
+		end else if ((&received_input1[4:0]) && (&received_input2[4:0])) begin
+			next_state = S1;
+		end else if ((&received_input1[4:0]) && !(&received_input2[4:0])) begin
+			next_state = S2;
+		end else if (!(&received_input1[4:0]) && (&received_input2[4:0])) begin
+			next_state = S3;
+		end else begin
+			next_state = S4;
+		end
+	end
+
+	always_ff (@ posedge clk) begin
+		if (reset) begin
+			state <= S0;
+		end else begin
+			state <= next_state;
+		end
+	end
+
 endmodule

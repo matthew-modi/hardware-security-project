cfa05b93e753fa912f7cb1f530356744556b037e
sps2308 <sps2308@cadpc35.ee.columbia.edu>
2025-05-06 19:34:44 -0400

    made buffer just a ff for now, doesn't incorporate stalling

--- buffer.v
+++ buffer.v
@@ -7,4 +7,14 @@ module buffer_slots (
     output wire        to_stall_mgmt
 );
     // Stall and Regular Slots 
+    logic [31:0] buffer_output;//outputs and inputs are wires
+    always_ff @ (posedge clk) begin
+		if (reset) begin
+			buffer_output <= 32'b0;
+		end else begin
+			buffer_output <= inputs;
+		end
+	end
+	assign outputs = buffer_output;
+
 endmodule
